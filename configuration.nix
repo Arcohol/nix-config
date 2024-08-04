e54972dc0c7de71377eb4473d5a540d7cc742e67
Tiantian Li <i@arcohol.com>
2024-08-05 03:24:33 +0800

    desktop: enable Wayland support for `sddm`

--- configuration.nix
+++ configuration.nix
@@ -27,6 +27,7 @@
 
   services.xserver.enable = true;
   services.displayManager.sddm.enable = true;
+  services.displayManager.sddm.wayland.enable = true;
   services.desktopManager.plasma6.enable = true;
 
   services.pipewire = {
