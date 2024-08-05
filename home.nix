cc7e04d83e4d9f93c298d93d1b2dad087680748f
Tiantian Li <i@arcohol.com>
2024-08-06 03:12:19 +0800

    desktop: add CJK fonts and mono fonts

--- home.nix
+++ home.nix
@@ -5,7 +5,18 @@
   home.homeDirectory = "/home/arcohol";
   home.stateVersion = "24.05";
 
-  home.packages = with pkgs; [ nixfmt-rfc-style ];
+  home.packages = with pkgs; [
+    # fonts
+    noto-fonts
+    noto-fonts-cjk-sans
+    noto-fonts-cjk-serif
+    noto-fonts-color-emoji
+    sarasa-gothic
+    hack-font
+    inter
+
+    nixfmt-rfc-style
+  ];
 
   programs.home-manager.enable = true;
 
@@ -24,5 +35,27 @@
   services.gpg-agent = {
     enable = true;
     pinentryPackage = pkgs.pinentry-curses;
+  };
+
+  fonts.fontconfig = {
+    enable = true;
+    defaultFonts = {
+      emoji = [ "Noto Color Emoji" ];
+      monospace = [
+        "Sarasa Term SC"
+        "Sarasa Term TC"
+        "Hack"
+      ];
+      sansSerif = [
+        "Inter"
+        "Noto Sans CJK SC"
+        "Noto Sans CJK TC"
+      ];
+      serif = [
+        "Noto Serif"
+        "Noto Serif CJK SC"
+        "Noto Serif CJK TC"
+      ];
+    };
   };
 }
