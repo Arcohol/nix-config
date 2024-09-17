b36e338286f3ec676577adebe9678aee10c455c2
Tiantian Li <i@arcohol.com>
2024-09-17 18:19:01 +0200

    chore: update flake.lock and fuck you Nvidia

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable-small";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
     disko.url = "github:nix-community/disko";
     disko.inputs.nixpkgs.follows = "nixpkgs";
