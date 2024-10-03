25fcf388166d2771d24fdcd8486a5cae3cabdb1a
Tiantian Li <i@arcohol.com>
2024-10-03 13:59:33 +0200

    feat: update boot loader configuration

--- hosts/common.nix
+++ hosts/common.nix
@@ -8,12 +8,19 @@
 {
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
+  boot.loader.systemd-boot.configurationLimit = 10;
   boot.kernelPackages = pkgs.linuxPackages_latest;
 
   nix.settings.experimental-features = [
     "nix-command"
     "flakes"
   ];
+  nix.settings.auto-optimise-store = true;
+  nix.gc = {
+    automatic = true;
+    dates = "weekly";
+    options = "--delete-older-than 1w";
+  };
 
   nixpkgs.config.allowAliases = false;
 
