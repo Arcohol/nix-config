5b32afc1eda4bc782d16f6134d296777f4f88bc7
Tiantian Li <i@arcohol.com>
2024-11-21 00:37:35 +0100

    Revert "chore: use nvidia beta driver"
    This reverts commit 3242509912f7fcef062c6edee225b87dcda3f444.

--- modules/nvidia.nix
+++ modules/nvidia.nix
@@ -1,9 +1,4 @@
-{ config, ... }:
-
 {
   services.xserver.videoDrivers = [ "nvidia" ];
-  hardware.nvidia = {
-    open = true;
-    package = config.boot.kernelPackages.nvidiaPackages.beta;
-  };
+  hardware.nvidia.open = true;
 }
