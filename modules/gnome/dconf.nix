cb386230d4a601d1665fc97df18b31126d20e3f3
Tiantian Li <i@arcohol.com>
2025-01-24 21:12:25 +0100

    chore: update dconf

--- modules/gnome/dconf.nix
+++ modules/gnome/dconf.nix
@@ -4,8 +4,6 @@
   dconf = {
     enable = true;
     settings = with lib.hm.gvariant; {
-      "org/gnome/desktop/interface".color-scheme = "prefer-dark";
-
       "org/gnome/desktop/background" = {
         picture-uri = "${./wallpaper.jpg}";
         picture-uri-dark = "${./wallpaper.jpg}";
@@ -17,14 +15,6 @@
         primary-color = "#241f31";
       };
 
-      "org/gnome/desktop/wm/preferences".button-layout = "appmenu:minimize,maximize,close";
-
-      "org/gnome/desktop/interface" = {
-        font-name = "Inter 11";
-        document-font-name = "Inter 11";
-        monospace-font-name = "Hack 10";
-      };
-
       "org/gnome/desktop/peripherals/mouse" = {
         speed = 0.2;
         accel-profile = "flat";
@@ -33,6 +23,56 @@
       "org/gnome/settings-daemon/plugins/color" = {
         night-light-enabled = true;
         night-light-temperature = mkUint32 3200;
+      };
+
+      "org/gnome/desktop/input-sources" = {
+        per-window = true;
+        sources = [
+          (mkTuple [
+            "ibus"
+            "rime"
+          ])
+        ];
+      };
+
+      "org/gnome/desktop/interface" = {
+        font-name = "Inter 11";
+        document-font-name = "Inter 11";
+        monospace-font-name = "Sarasa Mono SC 10";
+        color-scheme = "prefer-dark";
+        enable-hot-corners = false;
+        icon-theme = "Papirus-Dark";
+      };
+
+      "org/gnome/desktop/search-providers" = {
+        disable-external = true;
+      };
+
+      "org/gnome/desktop/wm/preferences" = {
+        button-layout = "appmenu:minimize,maximize,close";
+      };
+
+      "org/gnome/mutter" = {
+        dynamic-workspaces = true;
+      };
+
+      "org/gnome/shell" = {
+        enabled-extensions = [
+          "system-monitor@gnome-shell-extensions.gcampax.github.com"
+          "paperwm@paperwm.github.com"
+        ];
+        favorite-apps = [
+          "kitty.desktop"
+          "firefox.desktop"
+          "spotify.desktop"
+          "org.gnome.Nautilus.desktop"
+        ];
+      };
+
+      "org/gnome/shell/extensions/paperwm" = {
+        disable-scratch-in-overview = true;
+        only-scratch-in-overview = false;
+        show-window-position-bar = false;
       };
     };
   };
