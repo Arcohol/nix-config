9ab286888d0d18e4f6d57bac559e738da4cbde6e
Tiantian Li <i@arcohol.com>
2025-01-25 01:31:41 +0100

    Revert "chore: remove zed as i barely use it"
    This reverts commit 21cc8a03913ef52db3df040c81d344fd4c5a0280.

--- home/users/arcohol/default.nix
+++ home/users/arcohol/default.nix
@@ -71,6 +71,34 @@
     shellIntegration.mode = "no-cursor";
   };
 
+  programs.zed-editor = {
+    enable = true;
+    extensions = [
+      "github-theme"
+      "nix"
+    ];
+    userSettings = {
+      theme = "Github Dark";
+      vim_mode = true;
+      cursor_blink = false;
+      terminal = {
+        env = {
+          TERM = "xterm-256color";
+        };
+      };
+      git = {
+        inline_blame = {
+          enabled = false;
+        };
+      };
+      autosave = {
+        after_delay = {
+          milliseconds = 1000;
+        };
+      };
+    };
+  };
+
   services.gpg-agent = {
     enable = true;
     pinentryPackage = pkgs.pinentry-curses;
