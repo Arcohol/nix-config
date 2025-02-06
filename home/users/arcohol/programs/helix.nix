8088756b61c348ea9072f67ae37de0da51818d75
Tiantian Li <i@arcohol.com>
2025-02-06 19:21:58 +0100

    helix: add auto-save and cursor customization

--- home/users/arcohol/programs/helix.nix
+++ home/users/arcohol/programs/helix.nix
@@ -2,11 +2,21 @@
   programs.helix = {
     settings = {
       theme = "github_dark_transparent";
+      editor = {
+        auto-save = {
+          after-delay.enable = true;
+          after-delay.timeout = 1000;
+        };
+      };
     };
     themes = {
       github_dark_transparent = {
         "inherits" = "github_dark";
         "ui.background" = { };
+        "ui.cursor" = {
+          fg = "fg.default";
+          modifiers = [ "reversed" ];
+        };
       };
     };
   };
