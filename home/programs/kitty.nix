e0f21251657c3acc201d4ccc6d14708f8cdac56d
Tiantian Li <i@arcohol.com>
2025-02-16 00:00:49 +0100

    kitty: set tall layout

--- home/programs/kitty.nix
+++ home/programs/kitty.nix
@@ -4,6 +4,7 @@
       background_opacity = 0.8;
       font_size = 12;
       wayland_enable_ime = "no";
+      enabled_layouts = "tall:bias=60;full_size=1;mirrored=false";
     };
     shellIntegration.mode = "no-cursor";
   };
