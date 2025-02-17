e44577b90387d3e9bbb7bc14132114f9100ab3ca
Tiantian Li <i@arcohol.com>
2025-02-17 15:51:24 +0100

    chore: minor improvements

--- home/default.nix
+++ home/default.nix
@@ -1,11 +1,11 @@
 { lib, ... }:
 
 let
+  inherit (lib) hasSuffix;
+  inherit (builtins) filter attrNames readDir;
+
   nixFilesIn =
-    dir:
-    map (name: dir + "/${name}") (
-      builtins.filter (name: lib.hasSuffix ".nix" name) (builtins.attrNames (builtins.readDir dir))
-    );
+    dir: map (name: dir + "/${name}") (filter (name: hasSuffix ".nix" name) (attrNames (readDir dir)));
 in
 {
   imports = (nixFilesIn ./programs);
