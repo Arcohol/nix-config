318cd2827ea25f5566dc509a79550d56b8743c7a
Tiantian Li <i@arcohol.com>
2025-03-05 10:24:06 +0100

    fix: configure mpv wrapper with custom cursor settings

--- home/programs/mpv.nix
+++ home/programs/mpv.nix
@@ -1,8 +1,23 @@
+{ pkgs, ... }:
+
 {
   programs.mpv = {
+    package = (
+      pkgs.mpv-unwrapped.wrapper {
+        mpv = pkgs.mpv-unwrapped;
+        extraMakeWrapperArgs = [
+          "--set"
+          "XCURSOR_SIZE"
+          "24"
+          "--set"
+          "XCURSOR_THEME"
+          "Adwaita"
+        ];
+      }
+    );
     config = {
       profile = "high-quality";
-      autofit = "75%";
+      autofit = "90%";
       sub-auto = "fuzzy";
       audio-file-auto = "fuzzy";
       save-position-on-quit = true;
