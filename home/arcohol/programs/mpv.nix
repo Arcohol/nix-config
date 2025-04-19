10270fa8553140a9eb1b27ae8e48ea02805e692e
Tiantian Li <i@arcohol.com>
2025-04-19 22:02:23 +0200

    fix(mpv): correct autofit

--- home/arcohol/programs/mpv.nix
+++ home/arcohol/programs/mpv.nix
@@ -6,7 +6,7 @@
     scripts = with pkgs.mpvScripts; [ autoload ];
     config = {
       profile = "high-quality";
-      autofit = "90%";
+      autofit = "90%x90%";
       sub-auto = "fuzzy";
       audio-file-auto = "fuzzy";
       save-position-on-quit = true;
