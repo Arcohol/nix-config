b1eda364028728fb2265b3daa99e1f5b216e034d
Tiantian Li <i@arcohol.com>
2025-06-24 17:17:46 +0200

    Revert "desktop: use niri stable release"
    This reverts commit 932b4b6166fe5282acd188d938ea6c0228372b3a.

--- modules/niri/default.nix
+++ modules/niri/default.nix
@@ -6,7 +6,10 @@
   environment.variables.NIXOS_OZONE_WL = "1";
   services.displayManager.gdm.enable = true;
 
-  programs.niri.enable = true;
+  programs.niri = {
+    enable = true;
+    package = pkgs.niri-unstable;
+  };
   environment.systemPackages = with pkgs; [
     wl-clipboard
     xwayland-satellite-unstable
