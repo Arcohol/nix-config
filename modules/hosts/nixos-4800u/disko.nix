4cea1cecd9f4ea99b276ada971fa3b119dd2092b
Tiantian Li <i@arcohol.com>
2025-09-02 19:17:53 +0200

    hosts: add nixos-4800u

---
+++ modules/hosts/nixos-4800u/disko.nix
@@ -0,0 +1,59 @@
+{
+  flake.modules.nixos."hosts/nixos-4800u" = {
+    disko.devices = {
+      disk = {
+        main = {
+          device = "/dev/nvme0n1";
+          type = "disk";
+          content = {
+            type = "gpt";
+            partitions = {
+              ESP = {
+                size = "1G";
+                type = "EF00";
+                content = {
+                  type = "filesystem";
+                  format = "vfat";
+                  mountpoint = "/boot";
+                  mountOptions = [
+                    "fmask=0077"
+                    "dmask=0077"
+                  ];
+                };
+              };
+              nix = {
+                size = "200G";
+                content = {
+                  type = "filesystem";
+                  format = "ext4";
+                  mountpoint = "/nix";
+                };
+              };
+              persist = {
+                size = "400G";
+                content = {
+                  type = "filesystem";
+                  format = "ext4";
+                  mountpoint = "/persist";
+                };
+              };
+            };
+          };
+        };
+      };
+
+      nodev = {
+        "/" = {
+          fsType = "tmpfs";
+          mountOptions = [
+            "defaults"
+            "size=50%"
+            "mode=755"
+          ];
+        };
+      };
+    };
+
+    fileSystems."/persist".neededForBoot = true;
+  };
+}
---
+++ modules/hosts/nixos-4800u/hardware-configuration.nix
@@ -0,0 +1,27 @@
+{
+  flake.modules.nixos."hosts/nixos-4800u" =
+    {
+      config,
+      lib,
+      modulesPath,
+      ...
+    }:
+    {
+      imports = [ (modulesPath + "/installer/scan/not-detected.nix") ];
+
+      boot.initrd.availableKernelModules = [
+        "nvme"
+        "xhci_pci"
+        "usb_storage"
+        "sd_mod"
+      ];
+      boot.initrd.kernelModules = [ ];
+      boot.kernelModules = [ "kvm-amd" ];
+      boot.extraModulePackages = [ ];
+
+      networking.useDHCP = lib.mkDefault true;
+
+      nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
+      hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+    };
+}
