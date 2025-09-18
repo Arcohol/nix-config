2db85d7c9f785492511a2abcf508ff9fab9e34a1
Tiantian Li <i@arcohol.com>
2025-09-18 10:20:48 +0200

    desktop: add preservation directories for Bluetooth

--- modules/desktop/bluetooth.nix
+++ modules/desktop/bluetooth.nix
@@ -2,5 +2,9 @@
   flake.modules.nixos.desktop = {
     hardware.bluetooth.enable = true;
     services.blueman.enable = true;
+    preservation.preserveAt."/persist".directories = [
+      "/var/lib/bluetooth"
+      "/etc/bluetooth"
+    ];
   };
 }
