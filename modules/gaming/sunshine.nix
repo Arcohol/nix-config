980433624eb3d475249affda494ca8df0d92773b
Tiantian Li <i@arcohol.com>
2025-09-21 18:05:39 +0200

    desktop: auto start sunshine

--- modules/gaming/sunshine.nix
+++ modules/gaming/sunshine.nix
@@ -5,7 +5,6 @@
       services.sunshine = {
         enable = true;
         package = pkgs.sunshine.override { cudaSupport = true; };
-        autoStart = false;
         capSysAdmin = true; # needed for Wayland
         openFirewall = true;
       };
