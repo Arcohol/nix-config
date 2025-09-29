61f33553ae4b2deb1bb7af4ba1707458c1570cc2
Tiantian Li <i@arcohol.com>
2025-09-29 16:08:50 +0200

    desktop: update fontconfig

--- modules/desktop/fonts.nix
+++ modules/desktop/fonts.nix
@@ -2,84 +2,64 @@
   flake.modules.nixos.desktop =
     { pkgs, ... }:
     {
-      fonts.packages = with pkgs; [
-        noto-fonts
-        noto-fonts-cjk-sans
-        noto-fonts-cjk-serif
-        noto-fonts-color-emoji
-        sarasa-gothic
-        hack-font
-        inter
-        ibm-plex
-      ];
+      fonts = {
+        enableDefaultPackages = false;
+        packages = with pkgs; [
+          noto-fonts
+          noto-fonts-cjk-sans
+          noto-fonts-cjk-serif
+          noto-fonts-color-emoji
+          sarasa-gothic
+          inter
+        ];
+        fontconfig = {
+          defaultFonts = {
+            emoji = [ "Noto Color Emoji" ];
+            monospace = [
+              "Sarasa Mono Slab SC"
+              "Sarasa Mono Slab TC"
+              "Sarasa Mono Slab J"
+            ];
+            sansSerif = [
+              "Inter"
+              "Noto Sans CJK SC"
+              "Noto Sans CJK TC"
+              "Noto Sans CJK JP"
+            ];
+            serif = [
+              "Noto Serif"
+              "Noto Serif CJK SC"
+              "Noto Serif CJK TC"
+              "Noto Serif CJK JP"
+            ];
+          };
+          localConf = ''
+            <fontconfig>
+              <match target="pattern">
+                <test qual="any" name="family"><string>Arial</string></test>
+                <edit name="family" mode="assign" binding="same"><string>sans-serif</string></edit>
+              </match>
 
-      fonts.fontconfig = {
-        defaultFonts = {
-          emoji = [ "Noto Color Emoji" ];
-          monospace = [
-            "Sarasa Mono Slab SC"
-            "Hack"
-            "IBM Plex Mono"
-          ];
-          sansSerif = [
-            "Inter"
-            "Noto Sans CJK SC"
-            "Noto Sans CJK TC"
-          ];
-          serif = [
-            "Noto Serif"
-            "Noto Serif CJK SC"
-            "Noto Serif CJK TC"
-          ];
+              <match target="pattern">
+                <test qual="any" name="family"><string>PingFang SC</string></test>
+                <edit name="family" mode="assign" binding="same"><string>sans-serif</string></edit>
+              </match>
+
+              <match target="pattern">
+                <test qual="any" name="family"><string>Microsoft Sans Serif</string></test>
+                <edit name="family" mode="assign" binding="same"><string>sans-serif</string></edit>
+              </match>
+
+              <selectfont>
+                <rejectfont>
+                  <pattern>
+                    <patelt name="family"><string>DejaVu Sans</string></patelt>
+                  </pattern>
+                </rejectfont>
+              </selectfont>
+            </fontconfig>
+          '';
         };
-        localConf = ''
-          <fontconfig>
-          <match target="pattern">
-            <test qual="any" name="family"><string>Arial</string></test>
-            <edit name="family" mode="assign" binding="same"><string>sans-serif</string></edit>
-          </match>
-          <match target="pattern">
-            <test qual="any" name="family"><string>PingFang SC</string></test>
-            <edit name="family" mode="assign" binding="same"><string>sans-serif</string></edit>
-          </match>
-          <match target="pattern">
-            <test qual="any" name="family"><string>Microsoft Sans Serif</string></test>
-            <edit name="family" mode="assign" binding="same"><string>sans-serif</string></edit>
-          </match>
-          <match>
-            <test compare="contains" name="lang">
-            <string>zh_CN</string>
-            </test>
-            <edit mode="prepend" name="family">
-            <string>Noto Sans CJK SC</string>
-            </edit>
-          </match>
-          </fontconfig>
-        '';
       };
     };
-
-  flake.modules.homeManager.desktop = {
-    fonts.fontconfig = {
-      enable = true;
-      defaultFonts = {
-        emoji = [ "Noto Color Emoji" ];
-        monospace = [
-          "Sarasa Mono Slab SC"
-          "Hack"
-          "IBM Plex Mono"
-        ];
-        sansSerif = [
-          "Inter"
-          "Noto Sans CJK SC"
-          "Noto Sans CJK TC"
-        ];
-        serif = [
-          "Noto Serif"
-          "Noto Serif CJK SC"
-          "Noto Serif CJK TC"
-        ];
-      };
-    };
-  };
 }
