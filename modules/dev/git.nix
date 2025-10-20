e0847c912790d9f4ebe947da2afbe0b1bf64201a
Tiantian Li <i@arcohol.com>
2025-10-20 22:43:18 +0200

    git: update options to new syntax

--- modules/dev/git.nix
+++ modules/dev/git.nix
@@ -2,8 +2,10 @@
   flake.modules.homeManager.dev = {
     programs.git = {
       enable = true;
-      userName = "Tiantian Li";
-      userEmail = "i@arcohol.com";
+      settings = {
+        user.name = "Tiantian Li";
+        user.email = "i@arcohol.com";
+      };
       signing = {
         key = null;
         signByDefault = true;
