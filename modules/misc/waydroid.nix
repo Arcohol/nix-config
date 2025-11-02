78b4499db3a01373c2eb17fef0d11fcdb73097fb
Tiantian Li <i@arcohol.com>
2025-11-02 16:23:32 +0100

    waydroid: use nftables version

--- modules/misc/waydroid.nix
+++ modules/misc/waydroid.nix
@@ -1,8 +1,13 @@
 {
-  flake.modules.nixos.waydroid = {
-    virtualisation.waydroid.enable = true;
-    preservation.preserveAt."/persist".directories = [ "/var/lib/waydroid" ];
-  };
+  flake.modules.nixos.waydroid =
+    { pkgs, ... }:
+    {
+      virtualisation.waydroid = {
+        enable = true;
+        package = pkgs.waydroid-nftables;
+      };
+      preservation.preserveAt."/persist".directories = [ "/var/lib/waydroid" ];
+    };
 
   flake.modules.homeManager.waydroid = {
     home.persist = [ ".local/share/waydroid" ];
