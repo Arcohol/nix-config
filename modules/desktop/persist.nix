131512acd98754276e115007f1a4c9ef61f4e12f
Tiantian Li <i@arcohol.com>
2025-11-09 22:18:22 +0100

    refactor: consolidate persistence configuration

--- modules/desktop/persist.nix
+++ modules/desktop/persist.nix
@@ -2,34 +2,60 @@
   flake.modules.nixos.desktop =
     { config, ... }:
     {
-      preservation.preserveAt."/persist".users.arcohol.directories = [
-        "Desktop"
-        "Documents"
-        "Downloads"
-        "Music"
-        "Pictures"
-        "Public"
-        "Templates"
-        "Videos"
+      boot.initrd.systemd.enable = true;
+      preservation = {
+        enable = true;
+        preserveAt."/persist" = {
+          commonMountOptions = [ "x-gvfs-hide" ];
+          directories = [
+            {
+              directory = "/var/lib/nixos";
+              inInitrd = true;
+            }
+            "/var/log"
+            "/var/lib/systemd"
+            "/var/lib/NetworkManager"
+            "/var/lib/power-profiles-daemon"
+            "/etc/NetworkManager/system-connections"
+            "/root/.cache/nix"
+          ];
+          files = [
+            {
+              file = "/etc/machine-id";
+              inInitrd = true;
+            }
+          ];
 
-        "projects"
+          users.arcohol.directories = [
+            "Desktop"
+            "Documents"
+            "Downloads"
+            "Music"
+            "Pictures"
+            "Public"
+            "Templates"
+            "Videos"
 
-        ".config"
-        ".cache"
-        {
-          directory = ".gnupg";
-          mode = "0700";
-        }
-        {
-          directory = ".ssh";
-          mode = "0700";
-        }
-        {
-          directory = ".local/share/keyrings";
-          mode = "0700";
-        }
-      ]
-      ++ config.home-manager.users.arcohol.home.persist;
+            "projects"
+
+            ".config"
+            ".cache"
+            {
+              directory = ".gnupg";
+              mode = "0700";
+            }
+            {
+              directory = ".ssh";
+              mode = "0700";
+            }
+            {
+              directory = ".local/share/keyrings";
+              mode = "0700";
+            }
+          ]
+          ++ config.home-manager.users.arcohol.home.persist;
+        };
+      };
 
       systemd.suppressedSystemUnits = [ "systemd-machine-id-commit.service" ];
       systemd.tmpfiles.settings.preservation = {
