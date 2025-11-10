d99be1fab51a80ba9899e4bc266327909cf85edf
Tiantian Li <i@arcohol.com>
2025-11-10 15:50:15 +0100

    feat: add devShells

--- modules/flake-parts/default.nix
+++ modules/flake-parts/default.nix
@@ -5,13 +5,21 @@
     inputs.treefmt-nix.flakeModule
   ];
   systems = [ "x86_64-linux" ];
-  perSystem = {
-    treefmt = {
-      projectRootFile = "flake.nix";
-      programs.nixfmt = {
-        enable = true;
-        strict = true;
+  perSystem =
+    { pkgs, ... }:
+    {
+      treefmt = {
+        projectRootFile = "flake.nix";
+        programs.nixfmt = {
+          enable = true;
+          strict = true;
+        };
+      };
+      devShells.default = pkgs.mkShell {
+        buildInputs = with pkgs; [
+          nixd
+          nixfmt
+        ];
       };
     };
-  };
 }
