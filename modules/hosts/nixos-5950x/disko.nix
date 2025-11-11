63bb96e6b015db4175d09ad08697e4b7cfb26df5
Tiantian Li <i@arcohol.com>
2025-11-11 13:57:54 +0100

    fix: reference disks by id

--- modules/hosts/nixos-5950x/disko.nix
+++ modules/hosts/nixos-5950x/disko.nix
@@ -1,65 +1,70 @@
 {
-  flake.modules.nixos."hosts/nixos-5950x" = {
-    disko.devices = {
-      nodev."/" = {
-        fsType = "tmpfs";
-        mountOptions = [
-          "defaults"
-          "size=16G"
-          "mode=755"
-        ];
-      };
+  flake.modules.nixos."hosts/nixos-5950x" =
+    let
+      disk0 = "/dev/disk/by-id/nvme-WDC_PC_SN730_SDBPNTY-512G-1101_2027E4454310";
+      disk1 = "/dev/disk/by-id/nvme-Samsung_SSD_970_EVO_500GB_S5H7NS0N884601M";
+    in
+    {
+      disko.devices = {
+        nodev."/" = {
+          fsType = "tmpfs";
+          mountOptions = [
+            "defaults"
+            "size=16G"
+            "mode=755"
+          ];
+        };
 
-      disk.main = {
-        type = "disk";
-        device = "/dev/nvme0n1";
-        content = {
-          type = "gpt";
-          partitions = {
-            ESP = {
-              size = "1G";
-              type = "EF00";
-              content = {
-                type = "filesystem";
-                format = "vfat";
-                mountpoint = "/boot";
-                mountOptions = [ "umask=0077" ];
+        disk.main = {
+          type = "disk";
+          device = disk0;
+          content = {
+            type = "gpt";
+            partitions = {
+              ESP = {
+                size = "1G";
+                type = "EF00";
+                content = {
+                  type = "filesystem";
+                  format = "vfat";
+                  mountpoint = "/boot";
+                  mountOptions = [ "umask=0077" ];
+                };
               };
-            };
-            root = {
-              size = "100%";
-              content = {
-                type = "btrfs";
-                extraArgs = [
-                  "/dev/nvme1n1"
-                  "-f"
-                ];
-                subvolumes =
-                  let
-                    mountOptions = [ "compress=zstd" ];
-                  in
-                  {
-                    "@nix" = {
-                      mountpoint = "/nix";
-                      inherit mountOptions;
+              root = {
+                size = "100%";
+                content = {
+                  type = "btrfs";
+                  extraArgs = [
+                    disk1
+                    "-f"
+                  ];
+                  subvolumes =
+                    let
+                      mountOptions = [ "compress=zstd" ];
+                    in
+                    {
+                      "@nix" = {
+                        mountpoint = "/nix";
+                        inherit mountOptions;
+                      };
+                      "@persist" = {
+                        mountpoint = "/persist";
+                        inherit mountOptions;
+                      };
+                      "@swap" = {
+                        mountpoint = "/swap";
+                        inherit mountOptions;
+                        swap.swapfile.size = "16G";
+                      };
                     };
-                    "@persist" = {
-                      mountpoint = "/persist";
-                      inherit mountOptions;
-                    };
-                    "@swap" = {
-                      mountpoint = "/swap";
-                      inherit mountOptions;
-                      swap.swapfile.size = "16G";
-                    };
-                  };
+                };
               };
             };
           };
         };
       };
-    };
 
-    fileSystems."/persist".neededForBoot = true;
-  };
+      fileSystems."/persist".neededForBoot = true;
+    };
 }
