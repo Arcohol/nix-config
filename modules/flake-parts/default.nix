c7e93a7533130df1a49cf31a4581a0776c7611c0
Tiantian Li <i@arcohol.com>
2025-11-12 09:59:32 +0100

    Revert "feat: add devShells"
    This reverts commit d99be1fab51a80ba9899e4bc266327909cf85edf.

--- modules/flake-parts/default.nix
+++ modules/flake-parts/default.nix
@@ -5,21 +5,13 @@
     inputs.treefmt-nix.flakeModule
   ];
   systems = [ "x86_64-linux" ];
-  perSystem =
-    { pkgs, ... }:
-    {
-      treefmt = {
-        projectRootFile = "flake.nix";
-        programs.nixfmt = {
-          enable = true;
-          strict = true;
-        };
-      };
-      devShells.default = pkgs.mkShell {
-        buildInputs = with pkgs; [
-          nixd
-          nixfmt
-        ];
+  perSystem = {
+    treefmt = {
+      projectRootFile = "flake.nix";
+      programs.nixfmt = {
+        enable = true;
+        strict = true;
       };
     };
+  };
 }
