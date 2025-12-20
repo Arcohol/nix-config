c17dbfeb0df81a88d3f2ef85fcc166d8ee9167ef
Tiantian Li <i@arcohol.com>
2025-12-20 11:41:25 +0100

    development: streamline nrf

--- modules/development/nrf.nix
+++ modules/development/nrf.nix
@@ -2,27 +2,19 @@
   flake.modules.nixos.development =
     { pkgs, ... }:
     {
-      programs.nix-ld.enable = true;
       nixpkgs.config.segger-jlink.acceptLicense = true;
       nixpkgs.config.permittedInsecurePackages = [ "segger-jlink-qt4-874" ];
-      services.udev.packages = [
-        pkgs.nrf-udev
-        pkgs.segger-jlink
+      services.udev.packages = with pkgs; [
+        nrf-udev
+        segger-jlink
       ];
     };
 
-  flake.modules.homeManager.development =
-    { pkgs, ... }:
-    {
-      home.persist = [
-        "ncs"
-        ".cmake"
-      ];
-      home.packages' = with pkgs; [
-        {
-          package = nrfutil;
-          path = [ ".nrfutil" ];
-        }
-      ];
-    };
+  flake.modules.homeManager.development = {
+    home.persist = [
+      "ncs"
+      ".cmake"
+      ".nrfutil"
+    ];
+  };
 }
