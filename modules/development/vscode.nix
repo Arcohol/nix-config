b6e82a661d35bdb4018b372a0085b39ccab7267b
Tiantian Li <i@arcohol.com>
2026-02-09 11:46:39 +0100

    vscode: add commandLineArgs

--- modules/development/vscode.nix
+++ modules/development/vscode.nix
@@ -1,6 +1,11 @@
 {
-  flake.modules.homeManager.development = {
-    programs.vscode.enable = true;
-    home.persist = [ ".vscode" ];
-  };
+  flake.modules.homeManager.development =
+    { pkgs, ... }:
+    {
+      programs.vscode = {
+        enable = true;
+        package = pkgs.vscode.override { commandLineArgs = "--password-store=\"gnome-libsecret\""; };
+      };
+      home.persist = [ ".vscode" ];
+    };
 }
