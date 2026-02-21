afd3305351109601d1774c2317c7804c5ae9673a
Tiantian Li <i@arcohol.com>
2026-02-21 14:11:45 +0100

    nixos-m2: add home persist

--- modules/hosts/nixos-m2/persist.nix
+++ modules/hosts/nixos-m2/persist.nix
@@ -1,39 +1,39 @@
 {
-  flake.modules.nixos."hosts/nixos-m2" = {
-    boot.initrd.systemd.enable = true;
-    systemd.suppressedSystemUnits = [ "systemd-machine-id-commit.service" ];
-    preservation = {
-      enable = true;
-      preserveAt."/persist" = {
-        commonMountOptions = [ "x-gvfs-hide" ];
-        directories = [
-          {
-            directory = "/var/lib/nixos";
-            inInitrd = true;
-          }
-          "/var/log"
-          "/var/lib/systemd"
-          "/var/lib/NetworkManager"
-          "/etc/NetworkManager/system-connections"
-        ];
-        files = [
-          {
-            file = "/etc/machine-id";
-            inInitrd = true;
-          }
-          {
-            file = "/etc/ssh/ssh_host_rsa_key";
-            how = "symlink";
-          }
-          {
-            file = "/etc/ssh/ssh_host_ed25519_key";
-            how = "symlink";
-          }
-        ];
-        users.arcohol = {
-          directories = [ "projects" ];
+  flake.modules.nixos."hosts/nixos-m2" =
+    { config, ... }:
+    {
+      boot.initrd.systemd.enable = true;
+      systemd.suppressedSystemUnits = [ "systemd-machine-id-commit.service" ];
+      preservation = {
+        enable = true;
+        preserveAt."/persist" = {
+          commonMountOptions = [ "x-gvfs-hide" ];
+          directories = [
+            {
+              directory = "/var/lib/nixos";
+              inInitrd = true;
+            }
+            "/var/log"
+            "/var/lib/systemd"
+            "/var/lib/NetworkManager"
+            "/etc/NetworkManager/system-connections"
+          ];
+          files = [
+            {
+              file = "/etc/machine-id";
+              inInitrd = true;
+            }
+            {
+              file = "/etc/ssh/ssh_host_rsa_key";
+              how = "symlink";
+            }
+            {
+              file = "/etc/ssh/ssh_host_ed25519_key";
+              how = "symlink";
+            }
+          ];
+          users.arcohol.directories = config.home-manager.users.arcohol.home.persist ++ [ "projects" ];
         };
       };
     };
-  };
 }
