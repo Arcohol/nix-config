927fb7febf079f1d57287b2950de1f4564123bb8
Tiantian Li <i@arcohol.com>
2026-04-05 22:30:14 +0200

    helix: revert theme to catppuccin_mocha

--- modules/development/helix.nix
+++ modules/development/helix.nix
@@ -6,7 +6,7 @@
         enable = true;
         package = inputs.helix.packages.${pkgs.stdenv.hostPlatform.system}.default;
         settings = {
-          theme = "catppuccin_mocha_custom";
+          theme = "catppuccin_mocha";
           editor = {
             auto-save = {
               after-delay.enable = true;
@@ -15,12 +15,6 @@
           };
           keys.normal = {
             "C-S-i" = ":format";
-          };
-        };
-        themes = {
-          catppuccin_mocha_custom = {
-            "inherits" = "catppuccin_mocha";
-            "ui.background" = { };
           };
         };
       };
