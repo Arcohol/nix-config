6d74cf8bf06b391a2c274a525c6a2152d36b1b3e
Tiantian Li <i@arcohol.com>
2026-06-03 16:04:58 +0200

    nixos-m2: update imports

--- modules/hosts/nixos-m2/default.nix
+++ modules/hosts/nixos-m2/default.nix
@@ -7,8 +7,16 @@
         inputs.preservation.nixosModules.preservation
         inputs.nixos-apple-silicon.nixosModules.apple-silicon-support
       ]
-      ++ (with config.flake.modules.nixos; [ home-manager ]);
-      home-manager.users.arcohol.imports = with config.flake.modules.homeManager; [ home-manager ];
+      ++ (with config.flake.modules.nixos; [
+        home-manager
+        desktop
+        development
+      ]);
+      home-manager.users.arcohol.imports = with config.flake.modules.homeManager; [
+        home-manager
+        desktop
+        development
+      ];
 
       boot.loader.systemd-boot.enable = true;
       hardware.asahi.peripheralFirmwareDirectory = ./firmware;
