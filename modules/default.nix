fef20e4ae3591c2aed466976f58e964363227db1
Tiantian Li <i@arcohol.com>
2026-06-22 15:20:24 +0200

    refactor: add default.nix

--- modules/default.nix
+++ modules/default.nix
@@ -9,6 +9,27 @@ let
   collectHostsModules = modules: lib.filterAttrs (name: _: lib.hasPrefix prefix name) modules;
 in
 {
+  imports = [
+    inputs.flake-parts.flakeModules.modules
+    inputs.treefmt-nix.flakeModule
+  ];
+
+  systems = [
+    "x86_64-linux"
+    "aarch64-linux"
+    "aarch64-darwin"
+  ];
+
+  perSystem = {
+    treefmt = {
+      projectRootFile = "flake.nix";
+      programs.nixfmt = {
+        enable = true;
+        strict = true;
+      };
+    };
+  };
+
   flake.nixosConfigurations = lib.mapAttrs' (
     hostname: module:
     lib.nameValuePair (lib.removePrefix prefix hostname) (
