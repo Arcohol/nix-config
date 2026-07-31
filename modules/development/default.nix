7e590e7bf9aec4b403390511d913fba6f0ebc3c9
Tiantian Li <i@arcohol.com>
2026-07-31 12:59:40 +0200

    development: update segger-jlink

--- modules/development/default.nix
+++ modules/development/default.nix
@@ -1,7 +1,7 @@
 {
   flake.modules.nixos.development = { pkgs, ... }: {
     nixpkgs.config.segger-jlink.acceptLicense = true;
-    nixpkgs.config.permittedInsecurePackages = [ "segger-jlink-qt4-874" ];
+    nixpkgs.config.permittedInsecurePackages = [ "segger-jlink-qt4-952" ];
     services.udev.packages = with pkgs; [
       nrf-udev
       segger-jlink
