0486fd8f73f859d15a83a9e8b4794cb03df86971
Tiantian Li <i@arcohol.com>
2026-09-05 01:32:54 +0200

    merlot: use latest nvidia driver

--- modules/hosts/merlot/default.nix
+++ modules/hosts/merlot/default.nix
@@ -1,57 +1,64 @@
 { config, ... }: {
-  flake.modules.nixos."hosts/merlot" = { pkgs, ... }: {
-    imports = with config.flake.modules.nixos; [
-      home-manager
-      desktop
-      niri
-      gaming
-      development
-      waydroid
-    ];
-    home-manager.users.arcohol.imports = with config.flake.modules.homeManager; [
-      home-manager
-      desktop
-      niri
-      gaming
-      development
-      waydroid
-    ];
+  flake.modules.nixos."hosts/merlot" =
+    let
+      config' = config;
+    in
+    { config, pkgs, ... }: {
+      imports = with config'.flake.modules.nixos; [
+        home-manager
+        desktop
+        niri
+        gaming
+        development
+        waydroid
+      ];
+      home-manager.users.arcohol.imports = with config'.flake.modules.homeManager; [
+        home-manager
+        desktop
+        niri
+        gaming
+        development
+        waydroid
+      ];
 
-    networking.hostName = "merlot";
+      networking.hostName = "merlot";
 
-    services.openssh = {
-      enable = true;
-      settings.PasswordAuthentication = false;
-      settings.KbdInteractiveAuthentication = false;
-    };
+      services.openssh = {
+        enable = true;
+        settings.PasswordAuthentication = false;
+        settings.KbdInteractiveAuthentication = false;
+      };
 
-    services.xserver.videoDrivers = [ "nvidia" ];
-    hardware.nvidia.open = true;
+      services.xserver.videoDrivers = [ "nvidia" ];
+      hardware.nvidia = {
+        open = true;
+        package = config.boot.kernelPackages.nvidiaPackages.latest;
+      };
 
-    users = {
-      mutableUsers = false;
       users = {
-        root = {
-          hashedPassword = "$y$j9T$VaQvFqUam/c0UEzl0ngKl/$CyiFN/MyCaoBcEzT7MNrmSxJr.6/q08tPu7be4Sqx7.";
-          shell = pkgs.fish;
-        };
+        mutableUsers = false;
+        users = {
+          root = {
+            hashedPassword = "$y$j9T$VaQvFqUam/c0UEzl0ngKl/$CyiFN/MyCaoBcEzT7MNrmSxJr.6/q08tPu7be4Sqx7.";
+            shell = pkgs.fish;
+          };
 
-        arcohol = {
-          hashedPassword = "$y$j9T$XYq7YiTT1MU.RK.obCN/81$2EwL2m6ejAx7dP3yoLOEdBf6SzGIhCfitA/ZWx8U489";
-          isNormalUser = true;
-          extraGroups = [
-            "wheel"
-            "networkmanager"
-            "dialout"
-          ];
-          shell = pkgs.fish;
-          openssh.authorizedKeys.keys = [
-            "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOIHS9+5QgJfjis43PQ/UgDLy2ViHHBkHhl+N6f1DGfb highball"
-          ];
+          arcohol = {
+            hashedPassword = "$y$j9T$XYq7YiTT1MU.RK.obCN/81$2EwL2m6ejAx7dP3yoLOEdBf6SzGIhCfitA/ZWx8U489";
+            isNormalUser = true;
+            extraGroups = [
+              "wheel"
+              "networkmanager"
+              "dialout"
+            ];
+            shell = pkgs.fish;
+            openssh.authorizedKeys.keys = [
+              "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOIHS9+5QgJfjis43PQ/UgDLy2ViHHBkHhl+N6f1DGfb highball"
+            ];
+          };
         };
       };
-    };
 
-    system.stateVersion = "26.05";
-  };
+      system.stateVersion = "26.05";
+    };
 }
