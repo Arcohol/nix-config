d24475ccd3c4a5c15ba2cefb25ee6156e8a042aa
Tiantian Li <i@arcohol.com>
2026-09-08 12:41:46 +0200

    darwin: use chatgpt provided by nixpkgs

--- modules/development/default.nix
+++ modules/development/default.nix
@@ -27,8 +27,12 @@
           python3
           codex
         ]
-        ++ lib.optionals pkgs.stdenv.hostPlatform.isLinux [ nrfconnect ]
-        ++ (with inputs.llm-agents.packages.${pkgs.stdenv.hostPlatform.system}; [ chatgpt ]);
+        # darwin workaround: https://github.com/numtide/llm-agents.nix/issues/9046
+        ++ lib.optionals pkgs.stdenv.hostPlatform.isDarwin [ pkgs.chatgpt ]
+        ++ lib.optionals pkgs.stdenv.hostPlatform.isLinux [
+          nrfconnect
+          inputs.llm-agents.packages.${pkgs.stdenv.hostPlatform.system}.chatgpt
+        ];
 
       home.persist.directories = [
         ".arduino15"
